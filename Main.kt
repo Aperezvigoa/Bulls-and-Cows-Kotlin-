ee253b426cc1fec781011cbcd4559c28638e4feb
Alejandro Pérez-Vigo Albizu <108186387+Aperezvigoa@users.noreply.github.com>
2025-01-23 17:11:37 +0100

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -1,83 +1,106 @@
 package bullscows
 
-import kotlin.random.Random
-import kotlin.random.nextInt
-
-class Game (_player: Player, _maxAttemps: Int) {
-    var secretNumber = ""
-    val player = _player
-    val attemps = player.attemps.size
-    val maxAttemps = _maxAttemps
-    var cows: Int = 0
-    var bulls: Int = 0
-    private var _codeLength = 4
+class Game (playerOne: Player, maximumAttemps: Int) {
+    private var secretNumber = ""
+    private val player = playerOne
+    private val maxAttemps = maximumAttemps
+    private var cows: Int = 0
+    private var bulls: Int = 0
+    private var codeLength = 4
         set(value) {
-            if (value < 1 || value > 10)
+            if (value < 1 || value > 36)
                 println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.")
             else
                 field = value
         }
 
-    fun generateNumber() {
-        val tempList: MutableList<Int> = mutableListOf()
-        while (tempList.size < _codeLength) {
-            var randNum: Int
-            do {
-                randNum = Random.nextInt(0..9)
-                if (!tempList.contains(randNum))
-                    break
-            } while (true)
-            tempList.add(randNum)
+    private fun possibleSymbols(): Int {
+        println("Input the number of possible symbols in the code:")
+        return readln().toInt()
+    }
+
+    // Generate a list with numeric and letter characters with only the number of possible symbols
+    private fun generateSymbolList(): Set<Char> {
+        val availableSymbols: List<Char> = ('0'..'9') + ('a'..'z')
+        val randomSymbols: MutableSet<Char> = mutableSetOf()
+        val symbols = possibleSymbols()
+
+        while (randomSymbols.size < symbols) {
+            randomSymbols.add(availableSymbols.random())
         }
-        secretNumber = tempList.joinToString("")
+        return randomSymbols.toSet()
     }
 
-    fun selectingLength() {
-        println("Select the length of the secret number: ")
-        _codeLength = readln().toInt()
+    // Mix the list into a secret code
+    private fun generateCode() {
+        val randomSymbols = generateSymbolList()
+        secretNumber = randomSymbols.shuffled().take(codeLength).joinToString("")
     }
 
-    fun requestAttemp(): String {
-        val input = readln()
-        player.addAttemp(input)
-        return input
+    // Select the length of the code
+    private fun selectingLength() {
+        println("Please, enter the secret code's length: ")
+        codeLength = readln().toInt()
     }
 
+    // Check bulls into the user attemp
     private fun checkBulls(attemp: String): Int {
         var count: Int = 0
-        for (i in 0 until 4) {
+        for (i in 0 until codeLength) {
             if (attemp[i] == secretNumber[i])
                 count++
         }
         return count
     }
 
+    // Check cows into the user attemp
     private fun checkCows(attemp: String): Int {
         val userAttempList: List<Char> = attemp.toList()
         val secretWordList: List<Char> = secretNumber.toList()
         var count: Int = 0
-        for (i in 0 until 4) {
+        for (i in 0 until codeLength) {
             if (secretWordList.contains(userAttempList[i]))
                 count++
         }
         return count
     }
 
-    fun checkAttemp(attemp: String) {
+    // Calculate the bulls and cows into the user code
+    private fun evaluateAttemp(attemp: String) {
         bulls = checkBulls(attemp)
         cows = checkCows(attemp) - bulls
     }
 
-    fun grade() {
+    // return the feedback
+    private fun grade() {
         println(
             when {
                 cows == 0 && bulls == 0 -> "Grade: None. The secret code is $secretNumber."
-                bulls == 4 -> "Grade: Congrats! The secret code is $secretNumber."
-                bulls == 0 -> "Grade: $cows cow(s). The secret code is $secretNumber."
-                cows == 0 -> "Grade: $bulls bull(s). The secret code is $secretNumber."
+                bulls == codeLength -> "Grade: $bulls bulls!\nCongratulations! You guessed the secret code."
+                bulls == 0 -> "Grade: $cows cow(s)."
+                cows == 0 -> "Grade: $bulls bull(s)."
                 else -> "Grade: $bulls bull(s) and $cows cow(s). The secret code is $secretNumber."
             }
         )
+    }
+
+    fun play() {
+        selectingLength()
+        generateCode()
+        println("Okay, let's start a game!")
+        var attemps = 1
+        while (attemps <= maxAttemps) {
+            println("Turn ${attemps}:")
+            evaluateAttemp(player.requestAttemp())
+            grade()
+            if (bulls == codeLength)
+                break
+            else if (attemps == maxAttemps) {
+                println("You dont have more attemps...")
+                break
+            }
+            attemps++
+        }
     }
 }
 
@@ -88,6 +111,12 @@ class Player {
         }
     private val _attemps: MutableList<String> = mutableListOf()
     val attemps: List<String> get() = _attemps
+
+    fun requestAttemp(): String {
+        val input = readln()
+        addAttemp(input)
+        return input
+    }
 
     fun requestName() {
         println("Please, Write your name:")
@@ -101,9 +130,6 @@ class Player {
 
 fun main() {
     val jugador = Player()
-    val juego = Game(jugador, 1)
-
-    juego.selectingLength()
-    juego.generateNumber()
-    println("The random secret number is ${juego.secretNumber}.")
+    val juego = Game(jugador, 300)
+    juego.play()
 }
